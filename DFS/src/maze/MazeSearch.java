d04d3b1d2a4f09c29a4ad4d0ef41197e6a3e43da
Seth Shelnutt <shelnutt2@gmail.com>
2013-03-24 23:03:04 -0400

    Reworked into single file with both classes.

--- DFS/src/maze/MazeSearch.java
+++ DFS/src/maze/MazeSearch.java
@@ -3,7 +3,31 @@ package maze;
 
 import java.util.HashSet;
 
-public class DepthFirstSearch {
+public class MazeSearch {
+
+	private static Maze maze = new ExampleComplexMaze(); 
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		MazeCell fcell = searchMaze(maze);
+		
+		System.out.println("Congratulations - destination reached!");
+		System.out.println(fcell + " : " +fcell.isFinish());
+	}
+	
+	public static MazeCell searchMaze(Maze maze){
+		HashSet<MazeCell> beenthere = new HashSet<MazeCell>();
+		DepthFirstSearch find = new DepthFirstSearch(maze.getStart(),beenthere);
+		return find.getfinishcell();
+		
+		
+	}
+}
+
+class DepthFirstSearch {
     private MazeCell fcell;
 
 	public DepthFirstSearch(MazeCell cell, HashSet<MazeCell> beenthere){
